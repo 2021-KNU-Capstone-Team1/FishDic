b42c654952b3e3a52740f60e6f1feadf17649a5b
DESKTOP-2IQ9L0O\김형준 <hyung8789@naver.com>
2021-04-05 23:11:26 +0900

    2021-04-05 : 레이아웃 테스트

--- FishDic/app/src/main/java/com/knu/fishdic/MainActivity.java
+++ FishDic/app/src/main/java/com/knu/fishdic/MainActivity.java
@@ -1,14 +1,16 @@
 package com.knu.fishdic;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.setTitle(R.string.app_name);
+
+        setTitle(R.string.app_name);
     }
 }
