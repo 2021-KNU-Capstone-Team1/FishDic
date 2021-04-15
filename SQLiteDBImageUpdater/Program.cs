7eb658c78058362ceb2143acee02945a10277cb1
DESKTOP-2IQ9L0O\김형준 <hyung8789@naver.com>
2021-04-15 23:17:13 +0900

    2021-04-15 : 이미지 압축 및 DB 최적화 (VACCUM)

--- SQLiteDBImageUpdater/Program.cs
+++ SQLiteDBImageUpdater/Program.cs
@@ -2,16 +2,19 @@
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Drawing;
 
 namespace SQLiteDBImageUpdater
 {
     class Program
     {
-        public static string _targetDBPath = @"D:\FishDic\SQLiteDBImageUpdater\FishDicDB.db;"; //DB 경로
-        public static string _totalImagePath = @"D:\FishDic\SQLiteDBImageUpdater\images\"; //전체 이미지 저장 경로
+        public static string _targetDBPath = @"D:\FishDic\설계\DB\FishDicDB.db;"; //DB 경로
+        public static string _totalImagePath = @"D:\FishDic\설계\DB\images\"; //전체 이미지 저장 경로
         public static string _insertImageQuery = "UPDATE 어류_테이블 SET 이미지 = (@IMAGE) WHERE 이름 = (@IMAGE_NAME);";
+        public static long _imageCompressQuality = 75L; //이미지 압축 수준 (0 ~ 100)
 
-        //수정예정 : DB 용량 줄이기 위해 이미지 압축 수행
         static void Main(string[] args)
         {
             Console.WriteLine("타겟 DB 경로 : " + _targetDBPath);
@@ -31,19 +34,39 @@ namespace SQLiteDBImageUpdater
                 Console.Write("전체 : " + totalImageCount + " / 현재 : " + currentImageCount++);
 
                 string imageFileName = Path.GetFileNameWithoutExtension(imagePath);
-
                 FileStream fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
-                byte[] buffer = new byte[1024 * 1024]; //파일 스트림 처리를 위한 버퍼
+                FileInfo imageFileInfo = new FileInfo(imagePath);
+                
+                byte[] buffer = new byte[imageFileInfo.Length]; //읽어들인 이미지 버퍼
+                byte[] compressed_buffer; //압축 된 이미지 버퍼
 
-                while (fs.Position < fs.Length)
+                while (fs.Position < fs.Length) //이미지 크기만큼 읽는다.
                     fs.Read(buffer, 0, buffer.Length);
 
-                imageParam.Value = buffer;
+                //이미지 압축 수행
+                Image image;
+                using (var inputStream = new MemoryStream(buffer))
+                {
+                    image = Image.FromStream(inputStream);
+                    var jpegEncoder = ImageCodecInfo.GetImageDecoders()
+                      .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                    var encoderParameters = new EncoderParameters(1);
+                    encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, _imageCompressQuality);
+                    
+                    using (var outputStream = new MemoryStream())
+                    {
+                        image.Save(outputStream, jpegEncoder, encoderParameters);
+                        compressed_buffer = outputStream.ToArray();
+                    }
+                }
+
+
+                imageParam.Value = compressed_buffer;
                 imageNameParam.Value = imageFileName;
                 
-                Console.WriteLine(" (Target : " + imageFileName + ")");
+                Console.WriteLine(" (Target : " + imageFileName + ", Original Target Size : " + (buffer.Length - 1) + "bytes, Compressed Target Size : " + (compressed_buffer.Length - 1) + "bytes)");
 
-                //파라미터 추가 후
+                //파라미터 추가
                 cmd.Parameters.Add(imageParam);
                 cmd.Parameters.Add(imageNameParam);
 
---
+++ 설계/DB/assets으로 DB 복사.cmd
@@ -0,0 +1,4 @@
+@echo off
+PUSHD %~DP0
+set TARGET= ..\..\FishDic\app\src\main\assets
+copy /Y FishDicDB.db %TARGET%
