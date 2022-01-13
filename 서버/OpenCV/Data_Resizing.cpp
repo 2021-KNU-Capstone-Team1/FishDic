c24de0af95c0985521a643ef91e9526dcbe257a8
Kiwook Lee <57663465+wook0605@users.noreply.github.com>
2022-01-14 03:41:56 +0900

    Update Data_Resizing.cpp
    불필요한 코드 삭제 및 코드 수정

--- 서버/OpenCV/Data_Resizing.cpp
+++ 서버/OpenCV/Data_Resizing.cpp
@@ -1,13 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <Windows.h>
 
-
 using namespace std;
 using namespace cv;
+
 
 Mat rotate(Mat src, double angle) //회전 함수
 {
@@ -16,30 +18,37 @@ Mat rotate(Mat src, double angle) //회전 함수
 	Mat r = getRotationMatrix2D(pt, angle, 1.0); //회전 행렬 계산
 	warpAffine(src, dst, r, Size(src.cols, src.rows)); //어파인 변환 수행
 	return dst;
+}
 
+Mat Image_moving(Mat src, int tx, int ty) //이미지 평행 이동
+{
+	Mat dst = Mat();
+	Mat tmat = (Mat_<double>(2, 3) << 1, 0, tx, 0, 1, ty);; 
+	//
+	// 	   {1, 0, tx}
+	// 	   {0, 1, ty} 행렬 생성
+	warpAffine(src, dst, tmat, src.size()); // tx, ty만큼 평행이동하는 행렬
+	return dst;
 }
 
 int main()
 {
 	Mat img;
-	Mat resize_img;
 	Mat out_img_1;
 	Mat out_img_2;
-	string in_dir = "C:\\Users\\wook\\Desktop\\2차\\해삼\\"; //테스트 입력 디렉터리
-	string out_dir = "C:\\Users\\wook\\Desktop\\2차_출력\\해삼\\"; //테스트 출력 디렉터리
-	string name;
+	Mat resize_img;
+	string in_dir = "C:\\Users\\wook\\Desktop\\new_FishPhoto\\"; //테스트 입력 디렉터리
 	double angle = 0.0;
 	int j = 0;
-	int x = 590; //행
-	int y = 445; //열
-	//cout << "파일을 읽어올 디렉터리 경로를 입력하세요. ex)C:\\Users\\....\\>>";
-	//cin >> in_dir;
-	//cout << "파일을 출력할 디렉터리 경로를 입력하세요. ex)C:\\users\\....\\>>";
-	//cin >> out_dir;
+	int x = 300; //행
+	int y = 300; //열
 	vector<String> str; //파일 이름 저장할 벡터
+	vector<string> _dir;
 	char buf[256];
+	int count[4] = { 0, };
+	bool as = false;
 
-	glob(in_dir, str, false); //glob(찾을 파일 경로, 찾은 파일 경로, recuisive(T or F)
+	glob(in_dir, str, TRUE); //glob(찾을 파일 경로, 찾은 파일 경로, recuisive(T or F)
 		//T : 폴더 내 하위 폴더 속 까지 파일을 찾음
 		//F : 폴더 내 파일을 찾음
 	cout << "로드 개수 : " << str.size() << endl;
@@ -49,57 +58,73 @@ int main()
 		cout << "이미지가 존재하지 않습니다.\n";
 	}
 
-		for (int i = 0; i < str.size(); i++)
+	for (int i = 0; i < str.size(); i++) //이미지 처리
+	{
+		img = imread(str[i], IMREAD_COLOR);
+		resize(img, img, Size(x, y), 0, 0, CV_INTER_LINEAR); //이미지 리사이즈
+		ofstream in("C:\\Users\\wook\\Desktop\\처리한 데이터_좌우반전.txt");
+		ofstream delete_("C:\\Users\\wook\\Desktop\\삭제해야하는_데이터.txt");
+		char temp[100] = { NULL, };
+		strcpy(temp, str[i].c_str());
+		char* tok_dir = strtok(temp, "\\");
+		while (tok_dir != NULL)
 		{
-			img = imread(str[i], IMREAD_COLOR); //이미지 읽어오기
-			resize(img, resize_img, Size(x, y), 0, 0, CV_INTER_LINEAR); //파일 resize (선형 보간법 사용)
-
-			//out_img_1 = Labeling(resize_img, name);
-			for (angle = 0.0; angle < 360; angle += 5) //이미지 회전
+			tok_dir = strtok(NULL, "\\");
+			if (strcmp(tok_dir, "new_FishPhoto") == 0)
 			{
-				sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\2차_출력\\해삼\\%d.jpg", j); //파일저장위한 포멧 변경
-				//out_img_2 = rotate(out_img_1, angle); //이미지 회전
-				out_img_2 = rotate(resize_img, angle);
+				tok_dir = strtok(NULL, "\\");
+				break;
+			}
+		}
+		if (!img.empty())
+		{
+			//이미지 반전 처리부분
+			IplImage* img2 = new IplImage(img);
+			sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\new_FishPhoto_mirror\\%s\\%d.jpg", tok_dir, i);
+			cvFlip(img2, NULL, 1); //1 = 좌우반전 / -1 = 상하반전
+			img = cvarrToMat(img2, true);
+			imwrite(buf, img); //이미지 저장
+			delete img2;
+			cout << "파일이름 : " << str[i] << endl;
+			in << str[i] << "\n";
+			//이미지 이동 부분
+			for (int tx = 10; tx < 110; tx += 10) //오른쪽으로 밀기
+			{
+				sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\Fish_right\\%s\\%d.jpg", tok_dir,count[0]);
+				out_img_1 = Image_moving(img, tx, 0);
+				imwrite(buf, out_img_1);
+				count[0]++;
+				cout << str[i] << "이미지를" << tx << "만큼 오른쪽으로 이동시켰습니다.\n";
+			}
+			for (int ty = 10; ty < 110; ty += 10) //아래쪽으로 밀기
+			{
+				sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\Fish_down\\%s\\%d.jpg", tok_dir, count[1]);
+				out_img_1 = Image_moving(img, 0, ty);
+				imwrite(buf, out_img_1);
+				count[1]++;
+				cout << str[i] << "이미지를" << ty << "만큼 아래쪽으로 이동시켰습니다.\n";
+			}
+			for (int ty = -10; ty > -110; ty -= 10) //위쪽으로 밀기
+			{
+				sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\Fish_up\\%s\\%d.jpg", tok_dir, count[2]);
+				out_img_1 = Image_moving(img, 0, ty);
+				imwrite(buf, out_img_1);
+				count[2]++;
+				cout << str[i] << "이미지를" << ty << "만큼 위쪽으로 이동시켰습니다.\n";
+			}
+			//이미지 회전 부분
+			for (angle = 0.0; angle < 360; angle += 5) //이미지 5도씩 시계방향 회전
+			{
+				sprintf_s(buf, sizeof(buf), "C:\\Users\\wook\\Desktop\\new_FishPhoto\\%s\\%d.jpg",tok_dir, j); //파일저장위한 포멧 변경
+				out_img_2 = rotate(resize_img, angle); //이미지 회전
 				imwrite(buf, out_img_2); //이미지 저장
 				j++;
 			}
-			cout << i << "번째 이미지 사이즈변경 및 회전 완료\n";
 		}
-		return 0;
+		else
+		{
+			delete_ << str[i] << "\n";
+		}
 	}
 
-
-
-//Mat Labeling(Mat src, string name)
-//{
-//
-//	int j = 1;
-//	Mat img_gray;
-//	cvtColor(src, img_gray, COLOR_BGR2GRAY);
-//
-//	Mat img_threshold;
-//	threshold(img_gray, img_threshold, 100, 255, THRESH_BINARY_INV); //이미지 이진화
-//
-//	Mat img_labels, stats, centroids;
-//	int numOfLables = connectedComponentsWithStats(img_threshold, img_labels, stats, centroids, 8, CV_32S);
-//
-//	// 레이블링 결과에 사각형 그리고, 넘버 표시하기
-//	for (int j = 1; j < numOfLables; j++) {
-//		int area = stats.at<int>(j, CC_STAT_AREA);
-//		int left = stats.at<int>(j, CC_STAT_LEFT);
-//		int top = stats.at<int>(j, CC_STAT_TOP);
-//		int width = stats.at<int>(j, CC_STAT_WIDTH);
-//		int height = stats.at<int>(j, CC_STAT_HEIGHT);
-//
-//		if(width > 200 || height > 200 && left > 30 && top > 90 )
-//		{
-//			width = width + 30;
-//			rectangle(src, Point(left, top), Point(left + width, top + height), Scalar(0, 0, 255), 1); //Point(left, top) 시작점 좌표 ,  Point(left + width, top + height) 종료점 좌표
-//
-//			putText(src, "Scomber japonicus", Point(left + 20, top + 20), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 0, 0), 1); //텍스트 쓰는곳
-//		}
-//		
-//	}
-//	//cout << "numOfLables : " << numOfLables - 1 << endl;	// 최종 넘버링에서 1을 빼줘야 함
-//	return src;
-//}
+}
